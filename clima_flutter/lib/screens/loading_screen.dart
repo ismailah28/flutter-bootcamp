50531e7877e686c8afbf88076f843572b715853d
Ismaila Hassan <ismailah28@gmail.com>
2021-01-14 16:47:48 +0100

    Get weather data for current location and display

---
+++ clima_flutter/lib/screens/loading_screen.dart
@@ -0,0 +1,52 @@
+import 'package:clima_flutter/screens/location_screen.dart';
+import 'package:clima_flutter/services/networking.dart';
+import 'package:flutter/material.dart';
+import 'package:clima_flutter/services/location.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+const String apiKey = '67962a800ba7d140a874f94dc5b8d277';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => LocationScreen(
+          locationWeather: weatherData,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
+  }
+}
